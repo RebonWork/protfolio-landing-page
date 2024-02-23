54e2501739f7514ba556ad9c8fd6c50086308113
Rebon <rebonwork@gmail.com>
2024-02-23 16:19:01 +0200

    added heading gradient animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,20 +32,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "gradient": "gradient 6s linear infinite",
       },
-      // keyframes: {
-      //   text: {
-      //     "0%, 100%": {
-      //       "background-size": "200% 200%",
-      //       "background-position": "left center",
-      //     },
-      //     "50%": {
-      //       "background-size": "200% 200%",
-      //       "background-position": "right center",
-      //     },
-      //   },
-      // },
       colors: {
         text: "var(--text)",
         background: "var(--background)",
